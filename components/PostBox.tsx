60d8ad6df0225ae4af24981fa4bc6b8ec73926b3
rohangore1999 <gorerohan15@gmail.com>
2022-06-11 20:36:46 +0530

    changes in PostBox

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -76,6 +76,7 @@ function PostBox() {
                                 <div className='flex items-center px-2'>
                                     <p className='min-w-[90px]'>Image URL:</p>
                                     <input className='m-2 flex-1 bg-blue-50 p-2 outline-none' {...register('postImage')} type="text" placeholder='Optional...' />
+                                    
                                 </div>
                             </div>
                         )
